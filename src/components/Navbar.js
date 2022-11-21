e3eaba6c635d4239c55c2f529e05436700f3a506
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-21 06:01:47 +0000

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Navbar.modules.css';
+
+const Navbar = () => (
+  <div className="consDisplay">
+    <p>back key</p>
+    <h2>Components</h2>
+    <p>mic</p>
+    <p>settings</p>
+  </div>
+);
+
+export default Navbar;
