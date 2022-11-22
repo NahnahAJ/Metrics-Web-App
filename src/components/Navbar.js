c49782f43916b8e8eda9b44aaf0739d08d7a0633
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-22 14:37:54 +0000

    Implement back navigation functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Navbar.modules.css';
 
-const Navbar = () => (
-  <div className="consDisplay">
-    <p>back key</p>
-    <h2>Components</h2>
-    <p>mic</p>
-    <p>settings</p>
-  </div>
-);
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="consDisplay">
+      <button type="button" key={Date.now()} onClick={() => navigate(-1)}>back key</button>
+      <h2>Components</h2>
+      <p>mic</p>
+      <p>settings</p>
+    </div>
+  );
+};
 
 export default Navbar;
