99052960ffeccd673d64f9cff9d1b67e201a2c68
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-22 14:37:00 +0000

    Display data received from the API and implement search functionality

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -1,29 +1,68 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import Navbar from './Navbar';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getApiData } from '../redux/metrics';
 
 const Metrics = () => {
   const dispatch = useDispatch();
+  const artData = useSelector((state) => state.apiData.apiData);
+  const [searchFilter, setSearchFilter] = useState('');
+  const [searchResults, setSearchResults] = useState('');
 
-  useEffect(
-    () => {
-      dispatch(getApiData());
-    }, [dispatch],
-  );
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     // You can await here
-  //     const response = await MyAPI.getData(someId);
-  //     // ...
-  //   }
-  //   fetchData();
-  // }, [someId]); // Or [] if effect doesn't need props or state
+  const searchItems = (searchValue) => {
+    setSearchFilter(searchValue);
+    if (searchFilter !== '') {
+      const filteredData = artData.filter((item) => Object.values(item)
+        .join('')
+        .toLowerCase()
+        .includes(searchFilter.toLowerCase()));
+      setSearchResults(filteredData);
+    } else {
+      setSearchResults(artData);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getApiData());
+  }, [dispatch]);
 
   return (
     <>
-      <Navbar />
-      <div>Metrics</div>
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="search here"
+            onChange={(e) => searchItems(e.target.value)}
+          />
+        </div>
+
+        <div>
+          {searchFilter.length > 1 ? (
+            searchResults.map((key) => (
+              <div key={key.id}>
+                <img src={key.imageUrl} alt={key.image_id} />
+                <h3>{key.fileName}</h3>
+                <Link to={`details/${key.id}`}>
+                  <h2>{key.id}</h2>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div>
+              {artData.map((key) => (
+                <div key={key.id}>
+                  <img src={key.imageUrl} alt={key.image_id} />
+                  <h3>{key.fileName}</h3>
+                  <Link to={`details/${key.id}`}>
+                    <h2>{key.id}</h2>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
