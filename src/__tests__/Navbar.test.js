f063ebe3040afff55984e348ce8a9558e49a6916
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-23 23:03:29 +0000

    Fix linter errors

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,16 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/configureStore';
 import Navbar from '../components/Navbar';
-import { BrowserRouter } from 'react-router-dom';
 
 test('renders the Navbar component snapshot', () => {
   const { asFragment } = render(
     <BrowserRouter>
-    <Provider store={store}>
-      <Navbar />
-    </Provider>,
-    </BrowserRouter>
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
+      ,
+    </BrowserRouter>,
   );
   expect(asFragment()).toMatchSnapshot();
 });
