311cee32743e6f7f1776243f0f9cc1c5528db381
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-23 21:33:49 +0000

    Add classnames and elements to aid in the styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaAngleLeft, FaMicrophone } from 'react-icons/fa';
+import { AiOutlineSetting } from 'react-icons/ai';
 import '../styles/Navbar.modules.css';
+// FaSistrix search
 
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <div className="consDisplay">
-      <button type="button" key={Date.now()} onClick={() => navigate(-1)}>back key</button>
-      <h2>Components</h2>
-      <p>mic</p>
-      <p>settings</p>
-    </div>
+    <nav className="consDisplay displays">
+      <button className="backBtn iconSize" type="button" key={Date.now()} onClick={() => navigate(-1)} aria-label="Back"><FaAngleLeft /></button>
+      <h2>Vintage Collections</h2>
+      <div className="iconsCtn displays">
+        <span className="iconSize"><FaMicrophone /></span>
+        <span className="iconSize"><AiOutlineSetting /></span>
+      </div>
+    </nav>
   );
 };
 
